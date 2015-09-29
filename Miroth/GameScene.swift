a537d3ada9fd164cdb2e985d5e585f4c992507ac
Eric Ostrowski <ostrower@mail.gvsu.edu>
2015-09-28 22:02:04 -0500

    - Added new MapEditorScene class - Added trigger to switch to the map editor scene by pressing F1

--- Miroth/GameScene.swift
+++ Miroth/GameScene.swift
@@ -15,6 +15,12 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
+        let editorLabel = SKLabelNode(text: "F1: Open Map Editor")
+        editorLabel.fontSize = 10
+        editorLabel.fontColor = SKColor.blackColor()
+        editorLabel.fontName = "Courier"
+        editorLabel.position = CGPointMake(60, 405)
+        
         // Select the player sprite
         self.character = PlayerEntity()
         self.character!.position = CGPointMake(256, 256)
@@ -27,10 +33,19 @@ class GameScene: SKScene {
         secondTree.position = CGPointMake(300, 300)
         
         // Add the new nodes
+        self.addChild(editorLabel)
         self.addChild(tree)
         self.addChild(secondTree)
         self.addChild(self.character!)
         
+    }
+    
+    override func keyUp(theEvent: NSEvent) {
+        
+        // F1: Open map editor
+        if theEvent.keyCode == 122 {
+            self.scene?.view?.presentScene(MapEditorScene())
+        }
     }
     
     override func mouseDragged(theEvent: NSEvent) {
